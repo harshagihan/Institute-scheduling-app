1ffde02065b0f04f7f263de704fa28f4bf284188
Chehan-nethsara <48232659+Chehan-nethsara@users.noreply.github.com>
2021-05-18 03:04:59 +0530

    configurations

---
+++ src/migrations/20210408203621-create-working-data.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('WorkingData', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      day: {
+        type: Sequelize.INTEGER
+      },
+      time: {
+        type: Sequelize.STRING
+      },
+      isActive: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('WorkingData');
+  }
+};
