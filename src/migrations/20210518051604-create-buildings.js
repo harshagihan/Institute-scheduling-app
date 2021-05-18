faf15145bc21bb59a3202c629648f0607c737dd3
Harsha Gihan Madhushankha Liyanage <57912882+harshagihan@users.noreply.github.com>
2021-05-18 23:21:21 +0530

    location

---
+++ src/migrations/20210518051604-create-buildings.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Buildings', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      buildingName: {
+        type: Sequelize.STRING
+      },
+      center: {
+        type: Sequelize.INTEGER
+      },
+      isActive: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Buildings');
+  }
+};
